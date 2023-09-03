408ab23d0249087e671e9093ea42b7e7173b0cc8
Afiya2329 <142039360+Afiya2329@users.noreply.github.com>
2023-09-03 19:10:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var preloader = document.getElementById("loading");
 let api;
 function myFunction() {
          preloader.style.display = 'none';
-         setTimeout(50000);
+         setTimeout(preloader,50000);
          
     }
 
